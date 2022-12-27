3c568ac3aa084cab415f9cc58010afb16b81440c
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2022-12-27 10:31:43 +0000

    Add new message route

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,23 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Message Board', messages });
 });
 
+/* GET new message page */
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'New message form' });
+});
+
+/* POST new message */
+router.post('/new', function (req, res, next) {
+  const { author, message } = req.body;
+
+  const newMessage = {
+    user: author,
+    text: message,
+    added: getDateAsString(),
+  };
+
+  messages.push(newMessage);
+  res.redirect('/');
+});
+
 module.exports = router;
